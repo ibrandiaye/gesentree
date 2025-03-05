6622552da489df58c91569419d91166167a71b11
ibrandiaye <ibra789ndiaye@gmail.com>
2025-03-05 13:33:13 +0000

    dddd

--- resources/views/visiteur/add.blade.php
+++ resources/views/visiteur/add.blade.php
@@ -179,7 +179,7 @@
 
                 <!-- Start Content-->
                 <div class="container-fluid">
-                  div class="row">
+                  <div class="row">
 
                   <div class="col-12">
                       <div class="page-title-box">
