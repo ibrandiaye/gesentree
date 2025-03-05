60f5b4373b8c36089a2dd323afe4a8b7412fc0db
ibrandiaye <ibra789ndiaye@gmail.com>
2025-03-05 13:24:21 +0000

    dddd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -298,8 +298,8 @@
     </a>
  --}}
     <!-- Vendor js -->
-    <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
-
+{{--     <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+ --}}
     <!-- App js -->
     {{-- <script src="{{ asset('assets/js/app.min.js') }}"></script> --}}
     <script>
